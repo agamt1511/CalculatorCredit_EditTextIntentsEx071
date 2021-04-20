c079514adc022ec4073bed127817886da2757de0
at1511 <at1511@bs.amalnet.k12.il>
2021-04-20 20:38:52 +0300

    2

--- app/src/main/java/com/example/calculatorcredit_edittextintentsex071/MainActivity.java
+++ app/src/main/java/com/example/calculatorcredit_edittextintentsex071/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
@@ -23,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
     float sum = 0;
     String sumSt;
     int lis = 0;
-    int meq = 0;
 
 
     @Override
@@ -40,99 +40,174 @@ public class MainActivity extends AppCompatActivity {
         credits = (Button)findViewById(R.id.credits);
         et = (EditText)findViewById(R.id.et);
 
-        et.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                lis = 0;
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                lis = 1;
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                lis = 1;
-
-            }
-        });
-
     }
 
 
     public void add(View view) {
-        st = et.getText().toString();
-        et.getText().clear();
-        num1 = Float.parseFloat(st);
-        sum = sum + num1;
-        et.getText().clear();
+        if ((TextUtils.isEmpty(et.getText().toString()))) {
+            Toast.makeText(this, "ERROR, You did not enter input", Toast.LENGTH_LONG).show();
+        }
+        else {
+            if (lis != 0) {
+                if (checkPressed == 1) {
+                    sum = num1 + sum;
+                } else if (checkPressed == 2) {
+                    sum = num1 - sum;
+                } else if (checkPressed == 3) {
+                    sum = num1*sum;
+                } else if (checkPressed == 4) {
+                    if (st == "0") {
+                        Toast.makeText(this, "ERROR, Can not divide by zero", Toast.LENGTH_LONG).show();
+                    } else {
+                        sum = num1 / sum;
+                    }
+                }
+            }
 
-        if (lis == 1){
+            st = et.getText().toString();
             et.getText().clear();
+            num1 = Float.parseFloat(st);
+            sum = num1 + sum;
         }
         checkPressed = 1;
+        lis++;
     }
 
     public void subtract(View view) {
-        st = et.getText().toString();
-        et.getText().clear();
-        num1 = Float.parseFloat(st);
-        sum = sum - num1;
-        et.getText().clear();
+        if ((TextUtils.isEmpty(et.getText().toString()))) {
+            Toast.makeText(this, "ERROR, You did not enter input", Toast.LENGTH_LONG).show();
+        }
+        else {
+            if (lis != 0) {
+                if (checkPressed == 1) {
+                    sum = num1 + sum;
+                } else if (checkPressed == 2) {
+                    sum = num1 - sum;
+                } else if (checkPressed == 3) {
+                    sum = num1*sum;
+                } else if (checkPressed == 4) {
+                    if (st == "0") {
+                        Toast.makeText(this, "ERROR, Can not divide by zero", Toast.LENGTH_LONG).show();
+                    } else {
+                        sum = num1 / sum;
+                    }
+                }
+            }
 
-        if (lis == 1){
+            st = et.getText().toString();
             et.getText().clear();
+            num1 = Float.parseFloat(st);
+            sum = num1 - sum;
         }
         checkPressed = 2;
+        lis++;
     }
 
     public void multiply(View view) {
-        st = et.getText().toString();
-        et.getText().clear();
-        num1 = Float.parseFloat(st);
-        sum = sum * num1;
-        et.getText().clear();
+        if ((TextUtils.isEmpty(et.getText().toString()))) {
+            Toast.makeText(this, "ERROR, You did not enter input", Toast.LENGTH_LONG).show();
+        }
+        else {
+            if (lis != 0) {
+                if (checkPressed == 1) {
+                    sum = num1 + sum;
+                } else if (checkPressed == 2) {
+                    sum = num1 - sum;
+                } else if (checkPressed == 3) {
+                    sum = num1*sum;
+                } else if (checkPressed == 4) {
+                    if (st == "0") {
+                        Toast.makeText(this, "ERROR, Can not divide by zero", Toast.LENGTH_LONG).show();
+                    } else {
+                        sum = num1 / sum;
+                    }
+                }
+            }
 
-        if (lis == 1){
+            st = et.getText().toString();
             et.getText().clear();
+            num1 = Float.parseFloat(st);
+            if (sum != 0){
+                sum = sum *num1;
+            }
+            else {
+                sum = 1;
+                sum = sum *num1;
+            }
         }
         checkPressed = 3;
+        lis++;
     }
 
     public void todivide(View view) {
-        st = et.getText().toString();
-        et.getText().clear();
-        if (st == "0") {
-            Toast.makeText(this, "ERROR", Toast.LENGTH_SHORT).show();
+        if ((TextUtils.isEmpty(et.getText().toString()))) {
+            Toast.makeText(this, "ERROR, You did not enter input", Toast.LENGTH_LONG).show();
         }
         else {
-            num1 = Float.parseFloat(st);
-            sum = sum / num1;
-            et.getText().clear();
+            if (lis != 0) {
+                if (checkPressed == 1) {
+                    sum = num1 + sum;
+                } else if (checkPressed == 2) {
+                    sum = num1 - sum;
+                } else if (checkPressed == 3) {
+                    sum = num1*sum;
+                } else if (checkPressed == 4) {
+                    if (st == "0") {
+                        Toast.makeText(this, "ERROR, Can not divide by zero", Toast.LENGTH_LONG).show();
+                    } else {
+                        sum = num1 / sum;
+                    }
+                }
+            }
 
-            if (lis == 1){
-                et.getText().clear();
+            st = et.getText().toString();
+            et.getText().clear();
+            num1 = Float.parseFloat(st);
+            if (num1 != 0){
+                if (sum !=0){
+                    sum = sum/num1;}
+                else {
+                    sum = 1;
+                }
+            }
+            else {
+                Toast.makeText(this, "ERROR, You did not enter input", Toast.LENGTH_LONG).show();
             }
         }
         checkPressed = 4;
+        lis++;
     }
 
     public void delete(View view) {
-
-        int length = et.length();
-        et.getText().delete(0,length);
-
+        et.getText().delete(0,et.length());
         sum = 0;
+        checkPressed = 0;
+        lis = 0;
     }
 
     public void worth(View view) {
-
-        st = et.getText().toString();
-        et.getText().clear();
-        num1 = Float.parseFloat(st);
-        if (meq != 1) {
+        if ((TextUtils.isEmpty(et.getText().toString()))) {
+            Toast.makeText(this, "ERROR, You did not enter input", Toast.LENGTH_LONG).show();
+        }
+        else {
+            st = et.getText().toString();
+            et.getText().clear();
+            num1 = Float.parseFloat(st);
+            if (lis > 1) {
+                if (checkPressed == 1) {
+                    sum = sum + num1;
+                } else if (checkPressed == 2) {
+                    sum = sum - num1;
+                } else if (checkPressed == 3) {
+                    sum = sum * num1;
+                } else if (checkPressed == 4) {
+                    if (st == "0") {
+                        Toast.makeText(this, "ERROR, Can not divide by zero", Toast.LENGTH_LONG).show();
+                    } else {
+                        sum = sum / num1;
+                    }
+                }
+            }
             if (checkPressed == 1) {
                 sum = sum + num1;
             } else if (checkPressed == 2) {
@@ -140,21 +215,25 @@ public class MainActivity extends AppCompatActivity {
             } else if (checkPressed == 3) {
                 sum = sum * num1;
             } else if (checkPressed == 4) {
-                sum = sum / num1;
+                if (st == "0") {
+                    Toast.makeText(this, "ERROR, Can not divide by zero", Toast.LENGTH_LONG).show();
+                } else {
+                    sum = sum / num1;
+                }
             }
+
+            st = et.getText().toString();
+            sumSt = String.valueOf(sum);
+            et.getText().clear();
+            et.setText(sumSt);
+            checkPressed = 5;
         }
-        sumSt = String.valueOf(sum);
-        et.getText().clear();
-        et.setText(sumSt);
-        meq = 1;
     }
 
-    public void gocredits(View view) {
+
+    public void gocredit(View view) {
         Intent si = new Intent(this,SecondActivity2.class);
         si.putExtra("lastResult",sum);
         startActivity(si);
-
     }
-
 }
-
